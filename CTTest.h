69a0a1d86e22ff4276c186de074e99f784acaa13
Caleb <hktan_MBP@sarahsmacbook2.lan>
2014-03-02 12:01:18 +0100

    added a test class

---
+++ CTTest.h
@@ -0,0 +1,13 @@
+//
+//  CTTest.h
+//  Git Test
+//
+//  Created by Caleb on 02/03/14.
+//  Copyright (c) 2014 Caleb Tan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CTTest : NSObject
+
+@end
---
+++ CTTest.m
@@ -0,0 +1,13 @@
+//
+//  CTTest.m
+//  Git Test
+//
+//  Created by Caleb on 02/03/14.
+//  Copyright (c) 2014 Caleb Tan. All rights reserved.
+//
+
+#import "CTTest.h"
+
+@implementation CTTest
+
+@end
